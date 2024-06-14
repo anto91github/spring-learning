bb36fe630f29fde1af18c2bd1b86e11cfec2b05b
anto91github <antoz91@yahoo.com>
2024-06-14 14:45:42 +0700

    Delete SpringDemo/SpringDemo directory

--- SpringDemo/SpringDemo/src/main/java/com/anto/SpringDemo/Computer.java
+++
@@ -1,5 +0,0 @@
-package com.anto.SpringDemo;
-
-public interface Computer {
-	void compile();
-}
--- SpringDemo/SpringDemo/src/main/java/com/anto/SpringDemo/Laptop.java
+++
@@ -1,9 +0,0 @@
-package com.anto.SpringDemo;
-
-public class Laptop implements Computer
-{
-	public void compile()
-	{
-		System.out.println("Code Compiled in Laptop");
-	}
-}
